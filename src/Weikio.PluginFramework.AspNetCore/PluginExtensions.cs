2e7f1c9a83abdf64f55c695ed6eadeefbe137630
Thomas G <guenni37@gmail.com>
2021-05-27 14:29:59 +0200

    Update a test
    Update a test

--- src/Weikio.PluginFramework.AspNetCore/PluginExtensions.cs
+++ src/Weikio.PluginFramework.AspNetCore/PluginExtensions.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using Weikio.PluginFramework.Abstractions;
+using System.Linq;
 
 // ReSharper disable once CheckNamespace
 namespace Microsoft.Extensions.DependencyInjection
@@ -13,7 +14,40 @@ namespace Microsoft.Extensions.DependencyInjection
         
         public static T Create<T>(this Plugin plugin, IServiceProvider serviceProvider, params object[] parameters) where T : class
         {
-            return ActivatorUtilities.CreateInstance(serviceProvider, plugin, parameters) as T;
+            var result = default(T);
+
+            try
+            {
+                result = ActivatorUtilities.CreateInstance(serviceProvider, plugin, parameters) as T;
+            }
+            catch(Exception e)
+            {
+                var iVisualizationTypes = (from t in plugin.Assembly.GetExportedTypes()
+                                           where !t.IsInterface && !t.IsAbstract && !t.IsEnum
+                                           where typeof(T).IsAssignableFrom(t)
+                                           select t).ToArray();
+                var instantiatedTypes = iVisualizationTypes.Select(t =>
+                {
+                    var constructor = t.GetConstructors().OrderBy(c => c.GetParameters().Length).FirstOrDefault();
+                    if (constructor == null)
+                        throw new MissingMethodException("No public constructor defined for this object");
+                    var ctorParams = new object[constructor.GetParameters().Length];
+                    var cParams = constructor.GetParameters();
+                    for (var i = 0; i < cParams.Length; i++)
+                    {
+                        ctorParams[i] = serviceProvider.GetService(cParams[i].ParameterType);
+                    }
+                    var instance = constructor.Invoke(ctorParams);
+                    return instance;
+                });
+
+                if (instantiatedTypes != null)
+                {
+                    result = instantiatedTypes.First() as T;
+                }
+
+            }
+            return result;
         }
     }
 }
